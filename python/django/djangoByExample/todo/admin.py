d181dd8f3865a9fbf6cbc7901e5bf4a7b337a79e
Alexoner <onerhao@gmail.com>
2014-11-08 22:39:53 +0800

    todo:inline items

--- python/django/djangoByExample/todo/admin.py
+++ python/django/djangoByExample/todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from todo.models import Item
+from todo.models import Item, DateTime
 
 # Register your models here.
 
@@ -8,4 +8,14 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ["name", "priority", "difficulty", "created", "done"]
     search_fields = ["name"]
 
+
+class ItemInline(admin.TabularInline):
+    model = Item
+
+
+class DateAdmin(admin.ModelAdmin):
+    list_display = ["datetime"]
+    inlines = [ItemInline]
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(DateTime, DateAdmin)
