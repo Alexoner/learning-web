34d530401ae94ee8a877d29d90a948a20017d7c0
Alexoner <onerhao@gmail.com>
2014-11-08 21:53:33 +0800

    gtd:List model

--- python/django/gtd/todo/models.py
+++ python/django/gtd/todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class List(models.Model):
+    title = models.CharField(maxlength=250, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+    class Admin:
+        pass
