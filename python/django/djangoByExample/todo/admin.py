6e7bdc0cb59ea1b38f6cf4c77478ed3c115a1121
Alexoner <onerhao@gmail.com>
2014-11-08 22:28:56 +0800

    djangoByExample:add todo models

--- python/django/djangoByExample/todo/admin.py
+++ python/django/djangoByExample/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from todo import Item
 
 # Register your models here.
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "priority", "difficulty", "created", "done"]
+    search_fields = ["name"]
+
+admin.site.register(Item, ItemAdmin)
