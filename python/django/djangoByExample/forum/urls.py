3c1590b0f861566aaeb8409537ae44bb043d5f8f
Alexoner <onerhao@gmail.com>
2014-11-09 16:23:33 +0800

    forum:add explictly specify name argument in url() to resolve dictionary update error

--- python/django/djangoByExample/forum/urls.py
+++ python/django/djangoByExample/forum/urls.py
@@ -1,10 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from forum import views
 
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'djangoByExample.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-                       url(r"", "views.main", "main"),
-                       url(r'^forum/(\d+)/$', "forum"),
-                       url(r'^thread/(\d+)/$', "thread"),
+                       url(r"", views.main, name="main"),
+                       url(r'^forum/(\d+)/$', views.forum, name="forum"),
+                       url(r'^thread/(\d+)/$', views.thread, name="thread"),
+                       # url(r'^post/(new_thread|replay)/(\d+)/$',
+                       # views.post, 'post'),
+                       # url(r'^reply/(\d+)/$', views.reply),
+                       # url(r'^new_thread/(\d+)/$', 'new_thread'),
                        )
--- python/django/djangoByExample/forum/views.py
+++ python/django/djangoByExample/forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.core.urlresolvers import reverse
 from djangoByExample.settings import STATIC_URL
 
