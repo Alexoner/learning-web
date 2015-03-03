13ab54af4eb7566bd975f638719a61edb4326233
Alexoner <onerhao@gmail.com>
2015-03-03 19:53:53 +0800

    login action

---
+++ java/webx/webx-tutorial/src/main/java/com/alibaba/webx/tutorial1/app1/module/action/LoginAction.java
@@ -0,0 +1,20 @@
+import com.alibaba.citrus.turbine.Navigator;
+import com.alibaba.citrus.turbine.dataresolver.FormGroup;
+import com.alibaba.webx.tutorial1.app1.Visitor;
+
+public class LoginAction {
+
+    /*
+     * @FormGroup:annotation.Validate form and inject data from group
+     * into visitor
+     */
+    public void doCheck(@FormGroup("login")Visitor visitor,Navigator nav) {
+        String name = visitor.getName();
+        String passwd = visitor.getPasswd();
+        if("admin".equals(name)&&"admin".equals(passwd)) {
+            nav.redirectTo("app1Link").withTarget("form/welcome").withParameter("name",name);
+        }else {
+
+        }
+    }
+}
