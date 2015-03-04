d9d71817b0dd686551f0e08a55ad3eeba85c5bd1
Alexoner <onerhao@gmail.com>
2015-03-04 21:05:59 +0800

    format

--- java/mybatis/ibatisHelloWorld/src/main/java/com/howtodoinjava/ibatis/demo/dto/UserTEO.java
+++ java/mybatis/ibatisHelloWorld/src/main/java/com/howtodoinjava/ibatis/demo/dto/UserTEO.java
@@ -1,22 +1,23 @@
 package com.howtodoinjava.ibatis.demo.dto;
 
 public class UserTEO {
-    private Integer id;
-    private String name;
-    private String email;
-    private String password;
-    private int status;
 
-    public String getEmail(){
+    private Integer id;
+    private String  name;
+    private String  email;
+    private String  password;
+    private int     status;
+
+    public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
-        this.email=email;
+        this.email = email;
     }
 
     public void setId(Integer id) {
-        this.id=id;
+        this.id = id;
     }
 
     public String getName() {
@@ -24,22 +25,22 @@ public class UserTEO {
     }
 
     public void setName() {
-        this.name=name;
+        this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password){
-        this.password=password;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getStatus() {
         return status;
     }
 
-    public void setStatus() {
+    public void setStatus(int status) {
         this.status = status;
     }
 }
